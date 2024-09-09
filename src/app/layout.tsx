2150aad35ba93d857ca356d17fa63a7a75f90dba
bkcheung <b.cheung@hotmail.com>
2024-09-08 22:52:02 -0400

    ui: change header styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`font-serif antialiased`}>
-      <header className="flex items-center justify-evenly pt-12 pb-6 bg-gradient-to-r from-dark-green to-light-green">
+      <header className="flex items-center justify-evenly pt-12 pb-6 min-h-28 bg-gradient-to-r from-dark-green to-light-green">
           <button>
             <Image
               className=""
@@ -36,7 +36,7 @@ export default function RootLayout({
             />
           </button>
         </header>
-        <h2 className="p-4">Fresh roasted small-batch coffee beans</h2>
+        <h2 className="p-1 min-h-4 bg-[#EFF3EC] text-black text-center">Fresh roasted small-batch coffee beans</h2>
         {children}
       </body>
     </html>
