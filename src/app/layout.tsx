f41d68406677e2ec9afe97000b973c1b71beadc4
bkcheung <b.cheung@hotmail.com>
2024-09-09 16:11:51 -0400

    feat: add menu to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Image from "next/image";
+import Link from "next/link";
+import Menu from "./Menu";
 
 export const metadata: Metadata = {
   title: "Bean and Brew",
@@ -15,26 +17,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`font-serif antialiased`}>
-      <header className="flex items-center justify-evenly pt-12 pb-6 min-h-28 bg-gradient-to-r from-dark-green to-light-green">
-          <button>
-            <Image
-              className=""
-              src="/images/menu.png"
-              alt="menu logo"
-              width={30}
-              height={30}
-            />
-          </button>
-          <h3 className="text-3xl">Bean & Brew</h3>
-          <button>
-            <Image
-              className=""
-              src="/images/bag.png"
-              alt="bag logo"
-              width={30}
-              height={30}
-            />
-          </button>
+        <header className="flex items-center justify-around pt-12 pb-6 min-h-28 bg-gradient-to-r from-dark-green to-light-green">
+            {/* <MenuButton/> */}
+            <Menu/>
+            <Link href="/"className="text-3xl">Bean & Brew</Link>
+            <Link href="/cart">
+              <Image
+                className=""
+                src="/images/bag.png"
+                alt="bag logo"
+                width={30}
+                height={30}
+              />
+            </Link>
         </header>
         <h2 className="p-1 min-h-4 bg-[#EFF3EC] text-black text-center">Fresh roasted small-batch coffee beans</h2>
         {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function Home() {
   return (
       <main 
-        className="text-black bg-[url('/images/bkg.jpg')] bg-cover min-h-[calc(100vh-8rem)] 
-                   flex justify-center">
+        className="text-black bg-[url('/images/bkg.jpg')] bg-cover bg-center
+                   min-h-[calc(100vh-8rem)] flex justify-center">
         <Link 
           className="bg-white/80 text-center p-2 h-14 rounded-lg text-3xl w-6/12 mt-[60vh]"
           href="/shop"
