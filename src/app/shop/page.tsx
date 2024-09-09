f391390aad4bfc2d1bae8880cc91b2b501878324
bkcheung <b.cheung@hotmail.com>
2024-09-09 17:12:17 -0400

    feat: add server-side fetch for coffee bean information and display on shop page

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,7 +1,38 @@
-export default function Shop(){
+import Image from "next/image";
+export default async function Shop(){
+    const data = await fetch("https://fake-coffee-api.vercel.app/api");
+    const beans = await data.json();
+
+    // console.log(beans);
+    const coffee = beans.map((coffee:coffeeType)=>{
+        return(
+            <div key={coffee.id} className="text-black">
+                <Image src={coffee.image_url} alt={coffee.name} width={50} height={50}/>
+                <h2>{coffee.name}</h2>
+                <h1>${coffee.price}</h1>
+            </div>
+        )
+    })
+
     return(
-        <div className="text-black flex justify-center">
+        <div className="text-black flex flex-col justify-center">
            <h4 className="text-3xl py-8">Shop All</h4>
+           <div className="grid grid-cols-2 gap-4">
+             {coffee}
+           </div>
         </div>
     )
+}
+
+type coffeeType = {
+    id: number,
+    name: string,
+    description: string,
+    price: number,
+    region: string,
+    weight: number,
+    roast_level: number,
+    flavor_profile: string[],
+    grind_option: string[],
+    image_url: string
 }
