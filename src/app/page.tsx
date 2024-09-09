ecf914260d784347f9d2ec9216ba64a68c899e6a
bkcheung <b.cheung@hotmail.com>
2024-09-08 22:52:48 -0400

    feat: add shop link and updating styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 
+import Link from "next/link";
+
 export default function Home() {
   return (
-      <main className="text-black">
-        Home
+      <main 
+        className="text-black bg-[url('/images/bkg.jpg')] bg-cover min-h-[calc(100vh-8rem)] 
+                   flex justify-center">
+        <Link 
+          className="bg-white/80 text-center p-2 h-14 rounded-lg text-3xl w-6/12 mt-[60vh]"
+          href="/shop"
+        >Shop Now</Link>
       </main>
   );
 }
