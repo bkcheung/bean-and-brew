ad619494cbf28d5a1e1c071634d104e03cd278f9
bkcheung <b.cheung@hotmail.com>
2024-09-10 11:51:43 -0400

    feat: update coffee bean display in shop

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -2,22 +2,21 @@ import Image from "next/image";
 export default async function Shop(){
     const data = await fetch("https://fake-coffee-api.vercel.app/api");
     const beans = await data.json();
-
-    // console.log(beans);
     const coffee = beans.map((coffee:coffeeType)=>{
         return(
-            <div key={coffee.id} className="">
-                <Image src={coffee.image_url} alt={coffee.name} width={50} height={50}/>
+            <div key={coffee.id} className="flex flex-col items-center">
+                <div className="relative w-[12rem] h-[9rem]">
+                    <Image src={coffee.image_url} alt={coffee.name} fill={true}/>
+                </div>
                 <h2>{coffee.name}</h2>
                 <h1>${coffee.price}</h1>
             </div>
         )
     })
-
     return(
-        <div className=" flex flex-col justify-center">
+        <div className=" flex flex-col items-center">
            <h4 className="text-3xl py-8">Shop All</h4>
-           <div className="grid grid-cols-2 gap-4">
+           <div className="grid grid-cols-2 lg:grid-cols-4 gap-2">
              {coffee}
            </div>
         </div>
