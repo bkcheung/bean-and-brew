da86c85cf0fd4963ce2678212e2bfcd193cd410a
bkcheung <b.cheung@hotmail.com>
2024-09-10 12:39:50 -0400

    ui: update styling, header image, number of columns on resize

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
+
 export default async function Shop(){
     const data = await fetch("https://fake-coffee-api.vercel.app/api");
     const beans = await data.json();
     const coffee = beans.map((coffee:coffeeType)=>{
         return(
             <div key={coffee.id} className="flex flex-col items-center">
-                <div className="relative w-[12rem] h-[9rem]">
-                    <Image src={coffee.image_url} alt={coffee.name} fill={true}/>
+                <div className="relative w-[40vh] h-[30vh] ">
+                    <Image fill src={coffee.image_url} alt={coffee.name}
+                    sizes="(max-width: 768px) 50vw, (max-width: 1200px) 25vw"/>
                 </div>
                 <h2>{coffee.name}</h2>
                 <h1>${coffee.price}</h1>
@@ -16,7 +18,7 @@ export default async function Shop(){
     return(
         <div className=" flex flex-col items-center">
            <h4 className="text-3xl py-8">Shop All</h4>
-           <div className="grid grid-cols-2 lg:grid-cols-4 gap-2">
+           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
              {coffee}
            </div>
         </div>
