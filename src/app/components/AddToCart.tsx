548ac06934011277b1edab85289d267e8b7ae356
bkcheung <b.cheung@hotmail.com>
2024-09-10 23:03:04 -0400

    ui: change position of add to cart button and behaviour on hover

--- src/app/components/AddToCart.tsx
+++ src/app/components/AddToCart.tsx
@@ -4,7 +4,9 @@ const AddToCart = () => {
         console.log("add")
     }
     return(
-        <button className="bg-light-green p-2 rounded-lg"
+        // <button className="bg-light-green/50 p-2 m-2 rounded-md text-xs invisible group-hover:visible"
+        <button className="bg-white p-2 m-2 rounded-sm hover:bg-light-green/50 w-[75%]"
+                style={{border: '2px, outset, rgb(200 200 200 / 0.85)'}}
                 onClick={addToCart}>Add to Cart
         </button>
     )
--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import AddToCart from "../components/AddToCart";
 
 export default async function Shop(){
     const data = await fetch("https://fake-coffee-api.vercel.app/api");
@@ -8,12 +9,16 @@ export default async function Shop(){
     const coffee = filteredBeans.map((coffee:coffeeType)=>{
         return(
             <div key={coffee.id} className="flex flex-col items-center">
-                <Link href={`/shop/${coffee.id}`}className="relative w-[50vw] h-[45vh] md:w-[30vw] md:h-[42vh]">
-                    <Image fill src={coffee.image_url} alt={coffee.name}
-                       sizes="50vw" style={{'objectFit':"cover"}}/>
-                </Link>
-                <h2>{coffee.name}</h2>
+                <div className="group flex flex-col items-center">
+                    <Link href={`/shop/${coffee.id}`} 
+                        className="relative w-[50vw] h-[45vh] md:w-[30vw] md:h-[42vh]">
+                        <Image fill src={coffee.image_url} alt={coffee.name}
+                                sizes="50vw" style={{'objectFit':"cover"}}/>
+                    </Link>
+                    <h2 className="group-hover:text-dark-green group-hover:underline">{coffee.name}</h2>
+                </div>
                 <h1>${coffee.price}</h1>
+                <AddToCart/>
             </div>
         )
     })
