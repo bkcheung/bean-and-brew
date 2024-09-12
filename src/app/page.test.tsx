cc59f90507a8a8397c53b131ca98617be9faf196
bkcheung <b.cheung@hotmail.com>
2024-09-11 23:28:56 -0400

    test: update test suite name

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -2,7 +2,7 @@ import "@testing-library/jest-dom";
 import { render } from '@testing-library/react';
 import Home from "./page";
 
-describe('Menu', () => {
+describe('Home', () => {
     it('renders homepage',()=>{
         const home = render(<Home/>);
         expect(home).toMatchSnapshot;
