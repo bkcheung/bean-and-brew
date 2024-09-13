6179f8c770b0981a0e70dd76b800b5412610fe62
bkcheung <b.cheung@hotmail.com>
2024-09-12 21:02:08 -0400

    refactor: move Product element into separate module

---
+++ src/app/components/Product.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link"
+import Image from "next/image"
+import AddToCart from "./AddToCart"
+import { coffeeType } from "../utils/getBeans"
+
+interface productProps{
+    coffee: coffeeType
+}
+const Product = ({coffee}:productProps) => {
+    return(
+        <div key={coffee.id} className="flex flex-col items-center">
+            <div className="group flex flex-col items-center">
+                <Link href={`/shop/${coffee.id}`} 
+                    className="relative w-[50vw] h-[45vh] md:w-[30vw] md:h-[42vh]">
+                    <Image fill src={coffee.image_url} alt={coffee.name}
+                            sizes="50vw" style={{'objectFit':"cover"}}/>
+                </Link>
+                <h2 className="group-hover:text-dark-green group-hover:underline">{coffee.name}</h2>
+            </div>
+            <h1>${coffee.price}</h1>
+            <AddToCart id={coffee.id}/>
+        </div>
+    )
+}
+export default Product
--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,25 +1,10 @@
-import Image from "next/image";
-import Link from "next/link";
-import AddToCart from "../components/AddToCart";
 import { coffeeType, getBeans } from "../utils/getBeans";
+import Product from "../components/Product";
 
 export default async function Shop(){
     const filteredBeans = await getBeans();
     const coffee = filteredBeans.map((coffee:coffeeType)=>{
-        return(
-            <div key={coffee.id} className="flex flex-col items-center">
-                <div className="group flex flex-col items-center">
-                    <Link href={`/shop/${coffee.id}`} 
-                        className="relative w-[50vw] h-[45vh] md:w-[30vw] md:h-[42vh]">
-                        <Image fill src={coffee.image_url} alt={coffee.name}
-                                sizes="50vw" style={{'objectFit':"cover"}}/>
-                    </Link>
-                    <h2 className="group-hover:text-dark-green group-hover:underline">{coffee.name}</h2>
-                </div>
-                <h1>${coffee.price}</h1>
-                <AddToCart id={coffee.id}/>
-            </div>
-        )
+        return <Product key={coffee.id} coffee={coffee} />
     })
     return(
         <div className=" flex flex-col items-center">
