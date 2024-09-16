070b0e6fb82d95b920eb93e94d84a2886e9df97d
bkcheung <b.cheung@hotmail.com>
2024-09-16 18:14:53 -0400

    feat: display cart contents on cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,7 +1,21 @@
+'use client'
+import { useContext } from "react";
+import { CartContext } from "../context";
+
 export default function Cart() {
+  const {cartContent} = useContext(CartContext);
+  const userCart = cartContent.map((product: string, index:number)=>{
+    return(
+      <div key={index}>{product}</div>
+    )
+  })
+  console.log(cartContent);
   return (
-    <div className="text-black flex justify-center">
-      <h4 className="text-3xl py-8">Cart</h4>
-    </div>
+      <div className="text-black flex flex-col items-center">
+        <h4 className="text-3xl py-8">Cart</h4>
+        <div className="flex flex-col">
+          {userCart}
+        </div>
+      </div>  
   );
 }
