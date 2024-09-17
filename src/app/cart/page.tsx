2e9dd7f5a741d7f00af6bd0fff3c1d1785cfb806
bkcheung <b.cheung@hotmail.com>
2024-09-17 00:00:11 -0400

    feat: update cart to use bean data from context

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,20 +1,27 @@
-'use client'
-import { useContext } from "react";
+"use client";
+import { use, useContext } from "react";
 import { CartContext } from "../context";
+import { coffeeType } from "../utils/getBeans";
 
 export default function Cart() {
-  const {cartContent} = useContext(CartContext);
-  const userCart = cartContent.map((product: string, index:number)=>{
-    return(
-      <div key={index}>{product}</div>
-    )
-  })
+  const { cartContent, beans } = useContext(CartContext);
+  let coffeeBeans:coffeeType[];
+  if(beans!==null) coffeeBeans = use(beans);
+  else return new Error('Error; coffee beans not found');
+  let userCart;
+  if (cartContent.length == 0) {
+    userCart = <div>Cart is empty</div>;
+  } 
+  else {
+    userCart = cartContent.map((id: string, index: number) => {
+      const product = coffeeBeans.filter((bean:coffeeType) => bean.id === Number(id))[0];
+      return <div key={index}>{product?.name}</div>;
+    });
+  }
   return (
-      <div className="text-black flex flex-col items-center">
-        <h4 className="text-3xl py-8">Cart</h4>
-        <div className="flex flex-col">
-          {userCart}
-        </div>
-      </div>  
+    <div className="text-black flex flex-col items-center">
+      <h4 className="text-3xl py-8">Cart</h4>
+      <div className="flex flex-col">{userCart}</div>
+    </div>
   );
 }
