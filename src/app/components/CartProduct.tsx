d5e0952c3b9c4bd1b3ff96cd4f561eb3d3540828
bkcheung <b.cheung@hotmail.com>
2024-09-17 16:36:30 -0400

    feat: update CartProduct contents and styling

--- src/app/components/CartProduct.tsx
+++ src/app/components/CartProduct.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 import { coffeeType } from "../utils/getBeans";
 interface cartProps {
   coffee: coffeeType;
 }
 const CartProduct = ({ coffee }: cartProps) => {
   return (
-    <div className="flex items-center justify-around">
-      <div className="relative w-[30vw] h-[30vh]">
+    <div className="flex items-center justify-around rounded-xl bg-light-grey/50 m-4">
+      <div className="relative w-[30vw] h-[25vh]">
         <Image
           fill
           src={coffee.image_url}
@@ -15,11 +16,14 @@ const CartProduct = ({ coffee }: cartProps) => {
           style={{ objectFit: "cover" }}
         />
       </div>
-      <div className="flex flex-col">
-        <div>{coffee.name}</div>
-        <div className="flex">
+      <div className="flex flex-col m-4">
+      <Link href={`shop/${coffee.id}`} 
+            className="text-xl hover:text-dark-green hover:underline">
+            {coffee.name}
+      </Link>
+      <div className="flex text-lg my-4">
           <button onClick={() => console.log("- qty")}>-</button>
-          <div> 1 </div>
+          <div className="mx-4"> 1 </div>
           <button onClick={() => console.log("+ qty")}>+</button>
           <div className="ml-auto">{coffee.price}</div>
         </div>
