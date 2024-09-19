0fe6076081251ba463276a220690030575c23c4e
bkcheung <b.cheung@hotmail.com>
2024-09-19 18:23:11 -0400

    chore: clean up unused import

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { use, useContext } from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import EmptyCart from "../components/EmptyCart";
 import CartProduct from "../components/CartProduct";
 import { CartContext } from "../context";
