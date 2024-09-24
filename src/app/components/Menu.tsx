d5ff09da717a2051537a312d700ca3f0157ff737
bkcheung <b.cheung@hotmail.com>
2024-09-24 09:06:51 -0700

    feat: add invisible button that closes menu when user clicks anywhere outside the menu

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -12,22 +12,34 @@ const Menu = () => {
   return (
     <div className="z-50">
       <div
+        id="openMenu"
         className={cx(
-          "bg-white/90  fixed top-0 left-0 \
-                                min-h-[100vh] min-w-[75vw] flex flex-col justify-center text-3xl",
-          showMenu ? "show" : "hidden",
+          "flex fixed top-0 left-0 w-[100vw] h-[100vh]",
+          showMenu ? "" : "hidden",
         )}
-        aria-label="menu"
       >
-        <Link className="p-10" href="/" onClick={toggleMenu}>
-          Home
-        </Link>
-        <Link className="p-10" href="/shop" onClick={toggleMenu}>
-          Shop All
-        </Link>
-        <Link className="p-10" href="/cart" onClick={toggleMenu}>
-          Cart
-        </Link>
+        <div
+          className={cx(
+            "bg-white/90  fixed top-0 left-0 min-h-[100vh] w-[75vw] \
+            max-w-[375px] flex flex-col justify-center text-3xl",
+            showMenu ? "show" : "hidden",
+          )}
+          aria-label="menu"
+        >
+          <Link className="p-10" href="/" onClick={toggleMenu}>
+            Home
+          </Link>
+          <Link className="p-10" href="/shop" onClick={toggleMenu}>
+            Shop All
+          </Link>
+          <Link className="p-10" href="/cart" onClick={toggleMenu}>
+            Cart
+          </Link>
+        </div>
+        <button
+          className={cx("flex-1 cursor-default")}
+          onClick={toggleMenu}
+        ></button>
       </div>
       <button onClick={toggleMenu}>
         <Image src="/images/menu.png" alt="menu logo" width={30} height={30} />
