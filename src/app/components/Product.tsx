58414e1e6fdf497f986b7764a9bad1c331e937d5
bkcheung <b.cheung@hotmail.com>
2024-10-31 22:47:56 -0400

    fix: change image loading priorty to fix blurry image issue on prod deployment

--- src/app/components/Product.tsx
+++ src/app/components/Product.tsx
@@ -7,7 +7,6 @@ interface productProps {
   coffee: coffeeType;
 }
 const Product = ({ coffee }: productProps) => {
-  console.log(coffee.name, coffee.id, coffee.image_url);
   return (
     <div key={coffee.id} className="flex flex-col items-center">
       <div className="group flex flex-col items-center">
@@ -21,6 +20,7 @@ const Product = ({ coffee }: productProps) => {
             alt={coffee.name}
             sizes="50vw"
             style={{ objectFit: "cover" }}
+            priority
           />
         </Link>
         <h2 className="group-hover:text-dark-green group-hover:underline">
