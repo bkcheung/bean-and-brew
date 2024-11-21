2d0519cdeb4e4a75a6585f7f17b848fe59c32993
bkcheung <b.cheung@hotmail.com>
2024-11-20 21:22:55 -0500

    feat: update landing page text and background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,17 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main
-      className="bg-[url('/images/bkg.jpg')] bg-cover bg-center
-                   min-h-[calc(85vh-1rem)] flex justify-center"
-    >
+      className="bg-[url('/images/cup.jpg')] bg-cover bg-center
+      min-h-[calc(85vh-1rem)] flex justify-center">
+      <h4 className="absolute text-[2em] md:text-[2.5em] text-center top-[28vh]">did someone say</h4>
       <Link
-        className="bg-white/80 text-center p-2 h-14 rounded-lg text-3xl 
-                     min-w-[160px] w-[45vw] max-w-[250px] mt-[60vh]"
+        className="absolute bg-[#F4F3EE] hover:bg-[#E3E1D6] text-center p-2 h-14 rounded-lg text-xl 
+                   content-center w-[35vw] md:w-[15vw] lg:w-[12vw] shadow-lg top-[85vh]"
         href="/shop"
       >
-        Shop Now
+        Shop All
       </Link>
     </main>
+    
   );
 }
