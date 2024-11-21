64930af69fba7c73a42181dec6d8f5c476f12aa9
bkcheung <b.cheung@hotmail.com>
2024-11-21 12:15:38 -0500

    feat: update landing page to include product parallax

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,29 @@
 import Link from "next/link";
+import { getBeans } from "./utils/getBeans";
+import ProductParallax from "./components/ProductParallax";
 
-export default function Home() {
+export default async function Home() {
+  const beans = await getBeans();
   return (
     <main
-      className="bg-[url('/images/cup.jpg')] bg-cover bg-center
-      min-h-[calc(85vh-1rem)] flex justify-center">
-      <h4 className="absolute text-[2em] md:text-[2.5em] text-center top-[28vh]">did someone say</h4>
+      className="bg-[url('/images/cup.jpg')] bg-cover bg-center bg-fixed
+      min-h-[100vh] flex-col justify-center">
+      <h4 className="w-full fixed text-[2em] md:text-[2.5em] text-center top-[18%]">
+        did someone say
+      </h4>
       <Link
-        className="absolute bg-[#F4F3EE] hover:bg-[#E3E1D6] text-center p-2 h-14 rounded-lg text-xl 
-                   content-center w-[35vw] md:w-[15vw] lg:w-[12vw] shadow-lg top-[85vh]"
+        className="fixed bg-[#F4F3EE] hover:bg-[#E3E1D6] text-center p-2 h-14 rounded-lg text-xl 
+                   content-center w-[35vw] md:w-[15vw] lg:w-[12vw] shadow-lg top-[85%] left-1/2 -translate-x-1/2"
         href="/shop"
       >
         Shop All
       </Link>
+      <div className="h-[60vh] w-[100vw]" />
+      <ProductParallax beans={beans}/>
+      <div className="bg-[url('/images/silk.jpg')] bg-cover h-[20vh] w-[100vw] flex justify-center 
+                       align-text-bottom">
+        <h4 className="text-[2.5em] pt-auto pb-12 self-end">we got you.</h4>
+      </div>
     </main>
     
   );
