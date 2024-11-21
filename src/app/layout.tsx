17a264dc55eb0de56e36ed5d2a3a3b78ea5fbdc9
bkcheung <b.cheung@hotmail.com>
2024-11-21 12:18:24 -0500

    chore: remove scroll parallax provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "./components/Header";
 import { CartProvider } from "./context";
-import { Providers } from "./providers";
 import { getBeans } from "./utils/getBeans";
 
 export const metadata: Metadata = {
@@ -20,9 +19,7 @@ export default function RootLayout({
       <body className="font-serif antialiased">
         <CartProvider beans={getBeans()}>
           <Header />
-          <Providers>
             {children}
-          </Providers>
         </CartProvider>
       </body>
     </html>
