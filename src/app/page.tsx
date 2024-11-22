6ef3055e695c9cb5b04a01a0668d8370222a3a09
bkcheung <b.cheung@hotmail.com>
2024-11-22 01:05:41 -0500

    fix: reduce z-index of shop button so it scrolls behind header in mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default async function Home() {
         did someone say
       </h4>
       <Link
-        className="absolute md:fixed z-40 bg-[#F4F3EE] hover:bg-[#E3E1D6] text-center p-2 h-14 rounded-lg text-xl 
+        className="absolute md:fixed z-30 bg-[#F4F3EE] hover:bg-[#E3E1D6] text-center p-2 h-14 rounded-lg text-xl 
                    content-center w-[35vw] md:w-[15vw] lg:w-[12vw] shadow-lg top-[85%] left-1/2 -translate-x-1/2"
         href="/shop"
       >
