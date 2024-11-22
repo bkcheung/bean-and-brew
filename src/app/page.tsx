73691614fce7b4dca2ea280a5edff39f368fe958
bkcheung <b.cheung@hotmail.com>
2024-11-22 00:23:54 -0500

    feat: add handling for small screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,24 +7,19 @@ export default async function Home() {
   return (
     <main
       className="bg-[url('/images/cup.jpg')] bg-cover bg-center bg-fixed
-      min-h-[100vh] flex-col justify-center">
-      <h4 className="w-full fixed text-[2em] md:text-[2.5em] text-center top-[18%]">
+                 min-h-[100vh] w-[100vw] flex flex-col items-center">
+      <h4 className="absolute md:fixed w-full text-[2em] md:text-[2.5em] text-center pt-[3em] ">
         did someone say
       </h4>
       <Link
-        className="fixed z-40 bg-[#F4F3EE] hover:bg-[#E3E1D6] text-center p-2 h-14 rounded-lg text-xl 
+        className="absolute md:fixed z-40 bg-[#F4F3EE] hover:bg-[#E3E1D6] text-center p-2 h-14 rounded-lg text-xl 
                    content-center w-[35vw] md:w-[15vw] lg:w-[12vw] shadow-lg top-[85%] left-1/2 -translate-x-1/2"
         href="/shop"
       >
         Shop All
       </Link>
-      <div className="h-[60vh] w-[100vw]" />
+      <div className="h-[100vh] md:h-[30vh] w-[100vw]" />
       <ProductParallax beans={beans}/>
-      <div className="bg-[url('/images/silk.jpg')] bg-cover h-[20vh] w-[100vw] flex justify-center 
-                       align-text-bottom">
-        <h4 className="text-[2.5em] pt-auto pb-12 self-end">we got you.</h4>
-      </div>
     </main>
-    
   );
 }
