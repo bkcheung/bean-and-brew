39cbe31461719a3cfc51524ee44b81ccb9a04801
bkcheung <b.cheung@hotmail.com>
2025-01-24 00:01:29 -0500

    fix: adjust background image sizing based on window inner height

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,48 @@
+"use client";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import ProductParallax from "./ProductParallax";
+
+export default function Hero({ beans }: { beans: any }) {
+  const [showText, setShowText] = useState(true);
+  const [expandWidth, setExpandWidth] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      console.log(window.innerHeight);
+      if (window.innerHeight < 450) {
+        setShowText(false);
+      } else setShowText(true);
+      if (window.innerHeight < 510) {
+        setExpandWidth(true);
+      } else setExpandWidth(false);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [window.innerHeight]);
+
+  return (
+    <main
+      className={`bg-[url('/images/cup.jpg')] bg-[center_top_-5em] bg-no-repeat md:bg-cover md:bg-center md:bg-fixed
+                 min-h-[100vh] w-[100vw] flex flex-col items-center ${expandWidth ? "bg-[length:100vw_auto]" : "bg-[length:auto_100vh]"}`}
+    >
+      {showText && (
+        <h4 className="absolute md:fixed w-full text-[2em] md:text-[2.5em] text-center pt-[1.5em] ">
+          did someone say
+        </h4>
+      )}
+      <Link
+        className="absolute md:fixed z-30 bg-[#F4F3EE] hover:bg-[#E3E1D6] text-center p-2 h-14 rounded-lg text-xl 
+                   content-center w-[35vw] md:w-[15vw] lg:w-[12vw] shadow-lg top-[85%] left-1/2 -translate-x-1/2"
+        href="/shop"
+      >
+        Shop All
+      </Link>
+      <div className="h-[88vh] md:h-[5vh] w-[100vw]" />
+      <ProductParallax beans={beans} />
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,7 @@
-import Link from "next/link";
 import { getBeans } from "./utils/getBeans";
-import ProductParallax from "./components/ProductParallax";
+import Hero from "./components/Hero";
 
 export default async function Home() {
   const beans = await getBeans();
-  return (
-    <main
-      className="bg-[url('/images/cup.jpg')] bg-[length:auto_100vh] bg-[center_top_-5em] bg-no-repeat md:bg-cover md:bg-center md:bg-fixed
-                 min-h-[100vh] w-[100vw] flex flex-col items-center"
-    >
-      <h4 className="absolute md:fixed w-full text-[2em] md:text-[2.5em] text-center pt-[1.5em] ">
-        did someone say
-      </h4>
-      <Link
-        className="absolute md:fixed z-30 bg-[#F4F3EE] hover:bg-[#E3E1D6] text-center p-2 h-14 rounded-lg text-xl 
-                   content-center w-[35vw] md:w-[15vw] lg:w-[12vw] shadow-lg top-[85%] left-1/2 -translate-x-1/2"
-        href="/shop"
-      >
-        Shop All
-      </Link>
-      <div className="h-[88vh] md:h-[5vh] w-[100vw]" />
-      <ProductParallax beans={beans} />
-    </main>
-  );
+  return <Hero beans={beans} />;
 }
